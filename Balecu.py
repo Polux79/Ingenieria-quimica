4fe3b2ecca7ecd15203fb8a6b2b709112c80ff69
Henry Azula <henryazula@gmail.com>
2023-05-05 22:51:39 -0500

    Update Balecu.py

--- Balecu.py
+++ Balecu.py
@@ -1,19 +1,1 @@
-https://colab.research.google.com/drive/1Q7xaF467CUFrwQ27Ob5-jHgcK2vZZbQw?pli=1&authuser=1#scrollTo=Eqh9nO7HEASy
 
-#pip install chemics # Instalación del paquete chemics
-import chemics as cm
-
-#ce = cm.ChemicalEquation('2 HCl + 2 Na -> 2 NaCl + H2')
-ce = cm.ChemicalEquation('Cu(NO3)2 + 2 K -> 2 KNO3 + Cu')
-
-ce.balance  # Retorna True para una ecuación balanceada
-
-ce.rct_properties # Presenta un cuadro con las propiedades de los reactivos
-
-ce.prod_properties # Presenta un cuadro con las propiedades de los reactivos
-
-print("Reactivos")
-print(ce.rct_properties)
-
-print ("Productos")
-print (ce.prod_properties)
